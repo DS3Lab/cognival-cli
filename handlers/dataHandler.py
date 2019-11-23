40ee43b9a9bfc753a49429ba98c027b8ae0f22f1
advand <adrian.vanderlek@uzh.ch>
2019-11-23 15:52:08 +0100

    - updated config/example_1.json (mitchell -> zuco_eeg) - updated config/c.json (dito) - retained previous as example_1_original.json, c_original.json - added test_dataHandler - minor refactoring

--- handlers/dataHandler.py
+++ handlers/dataHandler.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+
 import pandas as pd
 import numpy as np
 import csv
 from sklearn.model_selection import KFold
 
-def chunker(path, wordEmbedding,name):
+def chunker(input_path_name,
+            output_path_name,
+            wordEmbedding,
+            name):
     chunk_number = 4
-    df_wE = pd.read_csv(path+wordEmbedding, sep=" ",
+    input_path = Path(input_path_name)
+    output_path = Path(output_path_name)
+    df_wE = pd.read_csv(input_path / wordEmbedding, sep=" ",
                         encoding="utf-8", quoting=csv.QUOTE_NONE)
     rows = df_wE.shape[0]
     chunk_size = rows // chunk_number
@@ -16,7 +23,7 @@ def chunker(path, wordEmbedding,name):
         if i == chunk_number - 1:
             end = end + rest
         df = df_wE.iloc[begin:end,:]
-        df.to_csv(path+name+str(i)+'.txt',sep=" ", encoding="utf-8")
+        df.to_csv(output_path / "{}_{}.txt".format(name, str(i)), sep=" ", encoding="utf-8")
 
 
 def update(df1, df2, on_column, columns_to_omit, whole_row):
@@ -39,7 +46,6 @@ def update(df1, df2, on_column, columns_to_omit, whole_row):
     return df1
 
 def dfMultiJoin(chunk_number, df_cD, df_wE):
-
     # Join from chunked Dataframe
     # Create chunks of df to perform 'MemorySafe'-join
     df_join = df_cD
@@ -54,10 +60,11 @@ def dfMultiJoin(chunk_number, df_cD, df_wE):
         else:
             if i == chunk_number - 1:
                 end = end + rest
-            update(df_join, df_wE.iloc[begin:end, :], on_column=['word'], columns_to_omit=df_cD.shape[1],whole_row=True)
+            update(df_join, df_wE.iloc[begin:end, :], on_column=['word'], columns_to_omit=df_cD.shape[1], whole_row=True)
+
+    return df_join
 
 def multiJoin(config, df_cD, wordEmbedding):
-
     # Join from chunked FILE
     df_join = df_cD
     chunk_number = config['wordEmbConfig'][wordEmbedding]["chunk_number"]
@@ -75,50 +82,8 @@ def multiJoin(config, df_cD, wordEmbedding):
     return df_join
 
 
-def dataHandler(config, wordEmbedding, cognitiveData, feature):
-
-    # READ Datasets into dataframes
-    df_cD = pd.read_csv(config['PATH'] + config['cogDataConfig'][cognitiveData]['dataset'], sep=" ")
-
-    # In case it's a single output cogData we just need the single feature
-    if config['cogDataConfig'][cognitiveData]['type'] == "single_output":
-        df_cD = df_cD[['word',feature]]
-    df_cD.dropna(inplace=True)
-
-    if (config['wordEmbConfig'][wordEmbedding]["chunked"]):
-        df_join = multiJoin(config,df_cD,wordEmbedding)
-    else:
-        df_wE = pd.read_csv(config['PATH'] + config['wordEmbConfig'][wordEmbedding]["path"], sep=" ",
-                            encoding="utf-8", quoting=csv.QUOTE_NONE)
-        # Left (outer) Join to get wordembedding vectors for all words in cognitive dataset
-        df_join = pd.merge(df_cD, df_wE, how='left', on=['word'])
-
-    df_join.dropna(inplace=True)
-
-    words = df_join['word']
-    words = np.array(words, dtype='str').reshape(-1,1)
-
-    df_join.drop(['word'], axis=1, inplace=True)
-
-    if config['cogDataConfig'][cognitiveData]['type'] == "single_output":
-        y = df_join[feature]
-        y = np.array(y, dtype='float').reshape(-1, 1)
-
-        X = df_join.drop(feature, axis=1)
-        X = np.array(X, dtype='float')
-    else:
-        features = df_cD.columns[1:]
-        y = df_join[features]
-        y = np.array(y, dtype='float')
-
-        X = df_join.drop(features, axis=1)
-        X = np.array(X, dtype='float')
-
-    return split_folds(words ,X,y, config["folds"], config["seed"] )
-
 def split_folds(words, X, y, folds, seed):
     '''
-
     :param words: np.array with words, same order, corresponding to X and y vectors
     :param X: np.array
     :param y: np.array
@@ -157,31 +122,73 @@ def split_folds(words, X, y, folds, seed):
     return words_test, X_train, y_train, X_test, y_test
 
 
+def dataHandler(config, wordEmbedding, cognitiveData, feature):
+    '''
+    :param config:
+    :param wordEmbedding:
+    :param cognitiveData:
+    :param feature:
+    '''
+    # READ Datasets into dataframes
+    df_cD = pd.read_csv(config['PATH'] + config['cogDataConfig'][cognitiveData]['dataset'], sep=" ")
+
+    # In case it's a single output cogData we just need the single feature
+    if config['cogDataConfig'][cognitiveData]['type'] == "single_output":
+        df_cD = df_cD[['word',feature]]
+    df_cD.dropna(inplace=True)
+
+    if (config['wordEmbConfig'][wordEmbedding]["chunked"]):
+        df_join = multiJoin(config, df_cD, wordEmbedding)
+    else:
+        df_wE = pd.read_csv(config['PATH'] + config['wordEmbConfig'][wordEmbedding]["path"], sep=" ",
+                            encoding="utf-8", quoting=csv.QUOTE_NONE)
+        # Left (outer) Join to get wordembedding vectors for all words in cognitive dataset
+        df_join = pd.merge(df_cD, df_wE, how='left', on=['word'])
+
+    df_join.dropna(inplace=True)
+
+    words = df_join['word']
+    words = np.array(words, dtype='str').reshape(-1,1)
+
+    df_join.drop(['word'], axis=1, inplace=True)
+
+    if config['cogDataConfig'][cognitiveData]['type'] == "single_output":
+        y = df_join[feature]
+        y = np.array(y, dtype='float').reshape(-1, 1)
+
+        X = df_join.drop(feature, axis=1)
+        X = np.array(X, dtype='float')
+    else:
+        features = df_cD.columns[1:]
+        y = df_join[features]
+        y = np.array(y, dtype='float')
+
+        X = df_join.drop(features, axis=1)
+        X = np.array(X, dtype='float')
+
+    return split_folds(words, X, y, config["folds"], config["seed"] )
+
+
 def main():
-    # import json
-    #
-    # with open('../config/setupConfig.json','r') as fr:
-    #     config = json.load(fr)
-    #
-    # we = 'elmo'
-    # feat = 'ALL_DIM'
-    # cds = []
-    #
-    # dim = [100, 500, 1000]
-    # file = 'mitchell-'
-    # for i in range(0, 9):
-    #     for j in dim:
-    #         cds.append(file+str(j)+'-'+str(i))
-    #
-    # for cd in cds:
-    #     dataHandler(config,we,cd,feat)
-    #     print("SUCCESS" + cd)
-
-
-    pass
+    import json
+    
+    with open('../config/example_1.json', 'r') as fr:
+        config = json.load(fr)
+    
+    we = 'glove-50'
+    feat = 'ALL_DIM'
+    cds = []
+    
+    #dim = [105]
+    #file_name = 'zuco_scaled'
+    #for i in range(0, 9):
+    #    for j in dim:
+    
+    cds = ['zuco-eeg']
+    
+    for cd in cds:
+        words_test, X_train, y_train, X_test, y_test = dataHandler(config, we, cd, feat)        
+        print("SUCCESS: " + cd)
 
 if __name__=="__main__":
     main()
-
-
-
