9968ab00fe8f66406e5682c9b4402e35116650e1
Adrian van der Lek <adrian.vanderlek@uzh.ch>
2020-01-30 17:33:20 +0100

    addendum

---
+++ cognival/lib_nubia/commands/messages.py
@@ -0,0 +1,17 @@
+LOGO_STR = """
+   ______                  _ _    __      __
+  / ____/___  ____ _____  (_) |  / /___ _/ /
+ / /   / __ \/ __ `/ __ \/ /| | / / __ `/ / 
+/ /___/ /_/ / /_/ / / / / / | |/ / /_/ / /  
+\____/\____/\__, /_/ /_/_/  |___/\__,_/_/   
+           /____/                           
+"""
+
+WELCOME_MESSAGE_STR = """
+Welcome to CogniVal! This tool allows you to evaluate word embeddings (predefined and custom) against the
+cognitive sources described in CogniVal: A Framework for Cognitive Word Embedding Evaluation (Hollenstein et al., 2019).
+
+Enter 'help' for a brief overview over all commands.
+Enter 'readme' to display the README.md.
+Enter 'welcome' to display this message.
+"""
