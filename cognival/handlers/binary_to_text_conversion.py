f0c664a83ea79e3095672b7fa717a147a174447b
Adrian van der Lek <adrian.vanderlek@uzh.ch>
2020-02-26 12:55:33 +0100

    removed bert-serving-server integration

--- cognival/handlers/binary_to_text_conversion.py
+++ cognival/handlers/binary_to_text_conversion.py
@@ -28,7 +28,7 @@ try:
 except ImportError:
     cprint('Warning: Package bert_serving.client not found. BERT conversion unavailable.', 'yellow')
 
-from prompt_toolkit.shortcuts import ProgressBar
+from prompt_toolkit.shortcuts import ProgressBar, message_dialog
 from termcolor import cprint
 
 from lib_nubia.prompt_toolkit_table import *
@@ -37,24 +37,15 @@ def bert_to_text(vocabulary_file, model_dir, output_path, num_worker):
     # Source: https://github.com/hanxiao/bert-as-service
     # Start Bert Model: bert-serving-start -model_dir /tmp/uncased_L-12_H-768_A-12/ -num_worker=4
     # model: 'base' (768 dimensions) or 'large' (1024 dimensions)
-   
-    # Create tmp directory if non-existent
-    try:
-        os.makedirs('tmp')
-    except FileExistsError:
-        pass
 
-    print('Starting BERT service server ...')
-    proc = Popen(['bert-serving-start',
-                  '-model_dir={}'.format(os.path.abspath(model_dir)),
-                  '-num_worker={}'.format(num_worker)],
-                  stdout=PIPE,
-                  stderr=PIPE,
-                  preexec_fn=os.setsid,
-                  cwd='tmp')
+    message_dialog(title='BERT binary conversion to word embeddings',
+                   text='BERT conversion requires that the server component of bert-as-service is running. Please execute\n'
+                        'the following command in a separate terminal and wait until the server reports "all set, ready to serve request!".\n'
+                        'Note that bert-as-service creates temporary files in the working directory the command is executed in, which\n'
+                        'must be removed manually after termination. The number of workers may be increased depending on available resources \n'
+                        'and ulimit restrictions.:\n'
+                        '\n bert-serving-start -model_dir {} -num_worker=1'.format(str(model_dir))).run()
 
-    print("Waiting for server readiness ...")
-    time.sleep(10)
 
     with open(vocabulary_file, 'r') as f:
         words = f.readlines()
@@ -64,7 +55,7 @@ def bert_to_text(vocabulary_file, model_dir, output_path, num_worker):
 
     with open(output_path, 'w') as embedding_file:
         # Load pre-trained model (weights)
-        with BertClient(ignore_all_checks=True) as bc:
+        with BertClient() as bc:
 
             count_not_found = 0
             print('Obtaining per-word embedding ...')
@@ -76,23 +67,9 @@ def bert_to_text(vocabulary_file, model_dir, output_path, num_worker):
                     print(word, embedding, file=embedding_file)
 
             print(count_not_found, ' words not found.')
-
-    # Terminate BERT server
-    print("Terminating BERT server ...")
-    os.killpg(proc.pid, signal.SIGTERM)
-
-    # Wait until processes terminated
-    while proc.poll() is None:
-        time.sleep(0.05)
-
-    print("Waiting for file lock release ...")
-    time.sleep(5)
-
-    print("Removing temporary directory")
-    try:
-        shutil.rmtree('tmp')
-    except:
-        cprint('Warning: Could not remove temporary directory "tmp" in the current working directory, manual removal necessary!', 'red')
+    
+    message_dialog(title='BERT binary conversion to word embeddings',
+                   text='BERT conversion complete. bert-as-service may now be safely terminated.').run()
 
 
 def elmo_to_text(vocabulary_file, output_path, layer='nocontext'):
